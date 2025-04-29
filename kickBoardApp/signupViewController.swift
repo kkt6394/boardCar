974c5aad5d6a475164b7cbf01f6c02ef05449862
choiyeonggeon <aws8792@naver.com>
2025-04-29 16:24:03 +0900

    feat : 회원가입 뷰에 기본적인 UI와 알럿 추가

--- kickBoardApp/signupViewController.swift
+++ kickBoardApp/signupViewController.swift
@@ -10,6 +10,7 @@ import SnapKit
 
 class signupViewController: UIViewController {
     
+    let fitstLabel = UILabel()
     let emailTextField = UITextField()
     let passwordTextField = UITextField()
     let nameTextField = UITextField()
@@ -23,8 +24,64 @@ class signupViewController: UIViewController {
     }
     
     func setupUI() {
-    
+        
+        fitstLabel.text = "회원가입을 위해 정보를 입력해주세요"
+        fitstLabel.textAlignment = .center
+        fitstLabel.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        
+        emailTextField.placeholder = "이메일"
+        emailTextField.borderStyle = .roundedRect
+        
+        passwordTextField.placeholder = "비밀번호"
+        passwordTextField.borderStyle = .roundedRect
+        passwordTextField.isSecureTextEntry = true
+        
+        nameTextField.placeholder = "이름"
+        nameTextField.borderStyle = .roundedRect
+        
+        signupButton.setTitle("회원가입", for: .normal)
+        signupButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        signupButton.addTarget(self, action: #selector(signup), for: .touchUpInside)
+
+        // 뷰에 추가
+        [fitstLabel, emailTextField, passwordTextField, nameTextField, signupButton].forEach {
+            view.addSubview($0)
+        }
+
+        // SnapKit 제약 설정
+        fitstLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(180)
+            make.leading.trailing.equalToSuperview().inset(30)
+        }
+
+        emailTextField.snp.makeConstraints { make in
+            make.top.equalTo(fitstLabel.snp.bottom).offset(20)
+            make.leading.trailing.equalToSuperview().inset(30)
+            make.height.equalTo(44)
+        }
+
+        passwordTextField.snp.makeConstraints { make in
+            make.top.equalTo(emailTextField.snp.bottom).offset(15)
+            make.leading.trailing.equalTo(emailTextField)
+            make.height.equalTo(44)
+        }
+
+        nameTextField.snp.makeConstraints { make in
+            make.top.equalTo(passwordTextField.snp.bottom).offset(15)
+            make.leading.trailing.equalTo(emailTextField)
+            make.height.equalTo(44)
+        }
+
+        signupButton.snp.makeConstraints { make in
+            make.top.equalTo(nameTextField.snp.bottom).offset(30)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(50)
+            make.width.equalTo(200)
+        }
     }
+
+    
+    
     
     @objc func signup() {
         guard let email = emailTextField.text,
@@ -57,14 +114,13 @@ class signupViewController: UIViewController {
             print("이름: \(user["name"] ?? "")")
         }
         
+        func showAlert(message: String) {
+            let alert = UIAlertController(title: "오류", message: message, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "완료", style: .default))
+            present(alert, animated: true)
+        }
+        
         // 회원가입 완료 후 로그인 화면으로 돌아가기
         dismiss(animated: true, completion: nil)
-    }
-
-    
-    func showAlert(message: String) {
-        let alert = UIAlertController(title: "오류", message: message, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "완료", style: .default))
-        present(alert, animated: true)
     }
 }
