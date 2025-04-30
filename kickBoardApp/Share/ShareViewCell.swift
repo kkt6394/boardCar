90adbb901047f68b684d67b4e3f9845b19ae4766
kimjaewoo522 <kimjaewoo522@gmail.com>
2025-04-30 22:16:51 +0900

    shareView 리팩토링

--- kickBoardApp/Share/ShareViewCell.swift
+++ kickBoardApp/Share/ShareViewCell.swift
@@ -1,97 +1,96 @@
+////
+////  ShareCell.swift
+////  kickBoardApp
+////
+////  Created by 김재우 on 4/28/25.
+////
 //
-//  ShareCell.swift
-//  kickBoardApp
+//import UIKit
+//import SnapKit
 //
-//  Created by 김재우 on 4/28/25.
+//class ShareViewCell: UITableViewCell {
+//    
+//    let kickBoardName = UITextField()
+//    let pickerView = UIPickerView()
+//    let sharedButton = UIButton()
+//    let dateLabel = UILabel()
+//    
+//    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+//        super.init(style: style, reuseIdentifier: reuseIdentifier)
+//        setUpCell()
+//        selectionStyle = .none
+//    }
 //
-
-import UIKit
-import SnapKit
-
-class ShareViewCell: UITableViewCell {
-    
-    public let kickBoardName = UITextField()
-    public let pickerView = UIPickerView()
-    public let sharedButton = UIButton()
-    public let dateLabel = UILabel()
-    
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        setUpCell()
-        selectionStyle = .none
-        
-//    sharedButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
-
-        
-    }
-
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        setUpCell()
-    }
-    
-    private func setUpCell() {
-        
-        backgroundColor = UIColor.sub3
-        
-        [kickBoardName, pickerView, sharedButton, dateLabel].forEach { contentView.addSubview($0) }
-        
-        // 킥보드 이름 UITextField
-        kickBoardName.leftView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 16.0, height: 0.0))
-        kickBoardName.leftViewMode = .always
-        kickBoardName.placeholder = "킥보드 이름"
-        kickBoardName.font = UIFont(name: "SUIT-Regular", size: 16)
-        kickBoardName.layer.cornerRadius = 13.0
-        kickBoardName.layer.borderWidth = 1.0
-        kickBoardName.layer.borderColor = UIColor.font3.cgColor
-        kickBoardName.backgroundColor = .white
-        kickBoardName.isUserInteractionEnabled = true
-        
-        // 공유 기간 UIPickerView
-        pickerView.backgroundColor = .white
-        pickerView.layer.borderWidth = 1.0
-        pickerView.layer.borderColor = UIColor.font3.cgColor
-        pickerView.layer.cornerRadius = 13.0
-        
-        // 내 킥보드 공유하기 UIButton
-        sharedButton.setTitle("내 킥보드 공유하기", for: .normal)
-        sharedButton.titleLabel?.font = UIFont(name: "SUIT-Heavy", size: 18)
-        sharedButton.backgroundColor = UIColor.main
-        sharedButton.layer.cornerRadius = 13.0
-        sharedButton.clipsToBounds = true
-        
-        dateLabel.text = "까지"
-        dateLabel.font = UIFont(name: "SUIT-Regular", size: 16)
-                            
-        kickBoardName.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(8)
-            $0.leading.trailing.equalToSuperview().inset(8)
-            $0.height.equalTo(36)
-        }
-        
-        pickerView.snp.makeConstraints {
-            $0.top.equalTo(kickBoardName.snp.bottom).offset(8)
-            $0.leading.equalToSuperview().inset(8)
-            $0.trailing.equalToSuperview().inset(48)
-            $0.height.equalTo(45)
-        }
-        
-        sharedButton.snp.makeConstraints {
-            $0.top.equalTo(pickerView.snp.bottom).offset(8)
-            $0.leading.trailing.equalToSuperview().inset(8)
-            $0.height.equalTo(41)
-        }
-        
-        dateLabel.snp.makeConstraints {
-            $0.leading.equalTo(pickerView.snp.trailing).offset(8)
-            $0.trailing.equalToSuperview().inset(8)
-            $0.centerY.equalTo(pickerView)
-        }
-        
-    }
-    
-    @objc func buttonTapped() {
-        print("공유하기 버튼이 눌렸습니다")
-    }
-    
-}
+//    required init?(coder: NSCoder) {
+//        super.init(coder: coder)
+//        setUpCell()
+//    }
+//    
+//    private func setUpCell() {
+//        
+//        backgroundColor = UIColor.sub3
+//        
+//        [kickBoardName, pickerView, sharedButton, dateLabel].forEach { contentView.addSubview($0) }
+//        
+//        // 킥보드 이름 UITextField
+//        kickBoardName.leftView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 16.0, height: 0.0))
+//        kickBoardName.leftViewMode = .always
+//        kickBoardName.placeholder = "킥보드 이름"
+//        kickBoardName.font = UIFont(name: "SUIT-Regular", size: 16)
+//        kickBoardName.layer.cornerRadius = 13.0
+//        kickBoardName.layer.borderWidth = 1.0
+//        kickBoardName.layer.borderColor = UIColor.font3.cgColor
+//        kickBoardName.backgroundColor = .white
+//        kickBoardName.isUserInteractionEnabled = true
+//        
+//        // 공유 기간 UIPickerView
+//        pickerView.backgroundColor = .white
+//        pickerView.layer.borderWidth = 1.0
+//        pickerView.layer.borderColor = UIColor.font3.cgColor
+//        pickerView.layer.cornerRadius = 13.0
+//        
+//        // 내 킥보드 공유하기 UIButton
+//        sharedButton.setTitle("내 킥보드 공유하기", for: .normal)
+//        sharedButton.titleLabel?.font = UIFont(name: "SUIT-Heavy", size: 18)
+//        sharedButton.backgroundColor = UIColor.main
+//        sharedButton.layer.cornerRadius = 13.0
+//        sharedButton.clipsToBounds = true
+//        
+////        sharedButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+//        
+//        dateLabel.text = "까지"
+//        dateLabel.font = UIFont(name: "SUIT-Regular", size: 16)
+//                            
+//        kickBoardName.snp.makeConstraints {
+//            $0.top.equalToSuperview().offset(8)
+//            $0.leading.trailing.equalToSuperview().inset(8)
+//            $0.height.equalTo(36)
+//        }
+//        
+//        pickerView.snp.makeConstraints {
+//            $0.top.equalTo(kickBoardName.snp.bottom).offset(8)
+//            $0.leading.equalToSuperview().inset(8)
+//            $0.trailing.equalToSuperview().inset(48)
+//            $0.height.equalTo(90)
+//        }
+//        
+//        sharedButton.snp.makeConstraints {
+//            $0.top.equalTo(pickerView.snp.bottom).offset(8)
+//            $0.leading.trailing.equalToSuperview().inset(8)
+//            $0.height.equalTo(41)
+//            $0.bottom.equalToSuperview().inset(8)
+//        }
+//        
+//        dateLabel.snp.makeConstraints {
+//            $0.leading.equalTo(pickerView.snp.trailing).offset(8)
+//            $0.trailing.equalToSuperview().inset(8)
+//            $0.centerY.equalTo(pickerView)
+//        }
+//        
+//    }
+//    
+////    @objc func buttonTapped() {
+////        print("공유하기 버튼이 눌렸습니다")
+////    }
+//    
+//}
--- kickBoardApp/Share/ShareViewController.swift
+++ kickBoardApp/Share/ShareViewController.swift
@@ -18,8 +18,8 @@ class ShareViewController: UIViewController {
     let months = Array(1...12).map { String(format: "%02d", $0) }
     let days = Array(1...31).map { String(format: "%02d", $0) }
     let years = Array(2000...2030).map { "\($0)" }
-
-
+    
+    
     override func loadView() {
         view = shareView
     }
@@ -27,104 +27,47 @@ class ShareViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        shareView.tableView.dataSource = self
-        shareView.tableView.delegate = self
-        shareView.tableView.register(ShareViewCell.self, forCellReuseIdentifier: "ShareViewCell")
-//         셀의 제약조건을 바탕으로 자동으로 계산
-        shareView.tableView.rowHeight = UITableView.automaticDimension
-//         UITableView가 초기 렌더링시 성능 최적화 보통(100..300)으로 설정
-        shareView.tableView.estimatedRowHeight = 200
-        shareView.tableView.isScrollEnabled = false
-        shareView.tableView.separatorStyle = .none
-        shareView.isUserInteractionEnabled = true
-        shareView.tableView.isUserInteractionEnabled = true
-
-//   키보드 관련
-//    NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-//    NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-//    
-//    let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
-//            view.addGestureRecognizer(tap)
-        }
-
-//
-//    @objc func keyboardWillShow(_ notification: Notification) {
-//            guard let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
-//            shareView.tableView.contentInset.bottom = keyboardFrame.height
-//        }
-//    
-//    @objc func keyboardWillHide(_ notification: Notification) {
-//            shareView.tableView.contentInset.bottom = 0
-//        }
-//    
-//    @objc func dismissKeyboard() {
-//            view.endEditing(true)
-//        }
-//
-//        deinit {
-//            NotificationCenter.default.removeObserver(self)
-//        }
+        shareView.pickerView.delegate = self
+        shareView.pickerView.dataSource = self
+    }
     
 }
-
-
     
-    
-
-    
-extension ShareViewController: UITableViewDelegate, UITableViewDataSource {
-        
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { 1 }
-        
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: "ShareViewCell", for: indexPath) as? ShareViewCell else {
-                return UITableViewCell()
-            }
-            
-            cell.pickerView.delegate = self
-            cell.pickerView.dataSource = self
-            cell.sharedButton.tag = indexPath.row
-            
-            return cell
-        }
-    }
-                  
 extension ShareViewController: UIPickerViewDelegate, UIPickerViewDataSource {
-        
-        func numberOfComponents(in pickerView: UIPickerView) -> Int {
-            return 3
-        }
-        
-        func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-            switch component {
-            case 0: return years.count
-            case 1: return months.count
-            case 2: return days.count
-            default: return 0
-            }
-        }
-        
-        func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
-            return pickerView.frame.width / 3
-        }
-        
-        func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
-            let label = UILabel()
-            label.textAlignment = .center
-            label.textColor = .black
-            label.font = UIFont(name: "SUIT-Regular", size: 16)
-            
-            switch component {
-            case 0: label.text = years[row] + "년"
-            case 1: label.text = months[row] + "월"
-            case 2: label.text = days[row] + "일"
-            default: label.text = ""
-            }
-            
-            label.backgroundColor = UIColor.white
-            
-            return label
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch component {
+        case 0: return years.count
+        case 1: return months.count
+        case 2: return days.count
+        default: return 0
         }
     }
-                  
+    
+    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+        return pickerView.frame.width / 3
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.textColor = .black
+        label.font = UIFont(name: "SUIT-Regular", size: 16)
+        
+        switch component {
+        case 0: label.text = years[row] + "년"
+        case 1: label.text = months[row] + "월"
+        case 2: label.text = days[row] + "일"
+        default: label.text = ""
+        }
+        
+        label.backgroundColor = UIColor.white
+        
+        return label
+    }
+}
+
